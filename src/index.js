de8c59ed8a7be805c650109e49b49bc58f2bd3fb
Urska Pangerc <urska@Mekic.local>
2017-02-20 14:44:06 +0100

    prevent modal from closing when back button is pressed- android

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ export default class Spinner extends React.Component {
     );
 
     return (
-      <Modal onRequestClose={() => this.close()} visible={visible} transparent>
+      <Modal onRequestClose={() => {}} visible={visible} transparent>
         {spinner}
       </Modal>
     );
